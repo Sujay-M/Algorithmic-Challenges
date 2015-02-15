8ff6dce6edb69d086006ab085570fc6979b6b4c5
sujay <sujaym.sujaym@gmail.com>
2015-02-15 20:39:19 +0530

    Solution using simple hashing(dictionaries in python)

--- 3.py
+++ 3.py
@@ -2,29 +2,17 @@
 def main():	
 	n,k = raw_input().split()
 	n,k = int(n),int(k)
-	l = raw_input().split()
-	l = [int(x) for x in l]
-	l.sort(reverse=True)
-	i = 0
-	j = 1
+	array = raw_input().split()
+	array = [int(x) for x in array]
 	count = 0
-	while(i<n):
-		if j==n:
-			i=i+1
-			j=i+1
-			continue
-		if l[i]-l[j]>k:
-			i=i+1
-			j=i+1
-			continue
-		if l[i]-l[j]==k:
-			count=count+1
-			j=j+1
-			continue
-		j=j+1
+	hashmap = {}
+	for num in  array:
+		hashmap[num] = 'True'
+	for num in array:
+		if hashmap.has_key(num-k):
+			count = count+1
 	return count
 if __name__=='__main__':
 	res = main()
 	print(res)
-
  
