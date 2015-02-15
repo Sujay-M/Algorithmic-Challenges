919546d9f1c1a508c194c96e314bd814b15c2281
sujay <sujaym.sujaym@gmail.com>
2015-02-15 19:20:36 +0530

    Solution to ques 3 altered

--- 3.py
+++ 3.py
@@ -4,8 +4,25 @@ def main():
 	n,k = int(n),int(k)
 	l = raw_input().split()
 	l = [int(x) for x in l]
-	res = [(x,y) for x in l for y in l if x-y==k]
-	return len(res)
+	l.sort(reverse=True)
+	i = 0
+	j = 1
+	count = 0
+	while(i<n):
+		if j==n:
+			i=i+1
+			j=i+1
+			continue
+		if l[i]-l[j]>k:
+			i=i+1
+			j=i+1
+			continue
+		if l[i]-l[j]==k:
+			count=count+1
+			j=j+1
+			continue
+		j=j+1
+	return count
 if __name__=='__main__':
 	res = main()
 	print(res)
